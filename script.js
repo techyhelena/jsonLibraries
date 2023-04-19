d5df130d2d6afd7e2cf9488cb06a7338cd32671c
Helen Zhou <fdr.helenzhou@gmail.com>
2023-04-18 23:15:19 -0400

    initial push

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+let memberDatabase = [
+{
+	"name": "Kim Chaewon",
+	"img_url": "",
+	"birthday": "August 1, 2000", 
+	"birthplace": "Seoul, South Korea",
+	"position": "Leader, Vocalist", 
+	"mbti": "ISTP",
+	"nationality": "Korean",
+	"hobby": "Watching dramas"
+},
+
+{
+	"name": "Miyawaki Sakura",
+	"img_url": "",
+	"birthday": "March 19, 1998", 
+	"birthplace": "Kagoshima City, Japan",
+	"position": "Vocalist", 
+	"mbti": "INFJ",
+	"nationality": "Japanese",
+	"hobby": "Watching dramas, reading books, guitar, and composing music"
+},
+{
+	"name": "Huh Yunjin",
+	"img_url": "",
+	"birthday": "October 8, 2001", 
+	"birthplace": "Seoul, South Korea (grew up in New York)",
+	"position": "Vocalist", 
+	"mbti": "INTP",
+	"nationality": "Korean-American",
+	"hobby": "Watching movies and playing games."
+}, 
+{
+	"name": "Nakamura Kazuha",
+	"img_url": "",
+	"birthday": "August 9, 2003", 
+	"birthplace": "Kochi, Japan (grew up in Osaka)",
+	"position": "Sub-Vocalist, Rapper, Dancer", 
+	"mbti": "INFP",
+	"nationality": "Japanese",
+	"hobby": "Watching Youtube video and internet shopping"
+},
+
+{
+	"name": "Hong Eunchae",
+	"img_url": "",
+	"birthday": "November 10, 2006", 
+	"birthplace": "Miryang, South Korea",
+	"position": "Vocalist, Lead Dancer, Maknae (Youngest)", 
+	"mbti": "ISFJ",
+	"nationality": "Korean",
+	"hobby": "Watching performances and mukbang videos"
+}
+]
+
+
+function updateInfo(incomingJSON) {
+	// Create whole content item div and set class
+
+}
