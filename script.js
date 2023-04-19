564a8a25cc2bed3b4437d634755c1b9d45525f58
Helen Zhou <fdr.helenzhou@gmail.com>
2023-04-19 10:42:41 -0400

    finished header, working on JSON portion

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
+let memberContainer = document.getElementById("memberContainer");
 
+// array of member info
 let memberDatabase = [
 {
 	"name": "Kim Chaewon",
-	"img_url": "",
+	"img_url": "chaewon.jpeg",
 	"birthday": "August 1, 2000", 
 	"birthplace": "Seoul, South Korea",
 	"position": "Leader, Vocalist", 
@@ -14,7 +16,7 @@ let memberDatabase = [
 
 {
 	"name": "Miyawaki Sakura",
-	"img_url": "",
+	"img_url": "sakura.jpeg",
 	"birthday": "March 19, 1998", 
 	"birthplace": "Kagoshima City, Japan",
 	"position": "Vocalist", 
@@ -24,7 +26,7 @@ let memberDatabase = [
 },
 {
 	"name": "Huh Yunjin",
-	"img_url": "",
+	"img_url": "yunjin.jpeg",
 	"birthday": "October 8, 2001", 
 	"birthplace": "Seoul, South Korea (grew up in New York)",
 	"position": "Vocalist", 
@@ -34,7 +36,7 @@ let memberDatabase = [
 }, 
 {
 	"name": "Nakamura Kazuha",
-	"img_url": "",
+	"img_url": "kazuha.jpeg",
 	"birthday": "August 9, 2003", 
 	"birthplace": "Kochi, Japan (grew up in Osaka)",
 	"position": "Sub-Vocalist, Rapper, Dancer", 
@@ -45,7 +47,7 @@ let memberDatabase = [
 
 {
 	"name": "Hong Eunchae",
-	"img_url": "",
+	"img_url": "eunchae.jpeg",
 	"birthday": "November 10, 2006", 
 	"birthplace": "Miryang, South Korea",
 	"position": "Vocalist, Lead Dancer, Maknae (Youngest)", 
@@ -55,8 +57,38 @@ let memberDatabase = [
 }
 ]
 
+for(var i = 0; i < memberDatabase.length; i++) {
+	updateInfo(memberDatabase[i]);
+}
+
+
 
 function updateInfo(incomingJSON) {
-	// Create whole content item div and set class
+	// create whole content item div and set class
+	let newContainer = document.createElement("DIV");
+	// setting class name
+	newContainer.classList.add("leftContainer");
 
+	// heading
+	let newContentHeading = document.createElement("H3");
+	newContentHeading.classList.add("name");
+	newContentHeading.innerText = incomingJSON['name'];
+	// adding heading to div
+	newContainer.appendChild(newContentHeading);
+
+	// image
+	let newContentImg = document.createElement("IMG");
+	newContentImg.classList.add('memberPic');
+	newContentImg.src = incomingJSON['image'];
+
+	newContainer.appendChild(newContentImg);
+
+	memberContainer.appendChild(newContainer);
 }
+
+
+
+
+
+
+
