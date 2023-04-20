fac0f5c0e71d6964e220410c7e2aa744ac83ccb7
Helen Zhou <fdr.helenzhou@gmail.com>
2023-04-19 22:31:02 -0400

    finished JSON, fixing layout

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
 let memberContainer = document.getElementById("memberContainer");
 
 // array of member info
 let memberDatabase = [
 {
 	"name": "Kim Chaewon",
-	"img_url": "chaewon.jpeg",
+	"img_url": "img/chaewon.jpeg",
 	"birthday": "August 1, 2000", 
 	"birthplace": "Seoul, South Korea",
 	"position": "Leader, Vocalist", 
@@ -16,7 +15,7 @@ let memberDatabase = [
 
 {
 	"name": "Miyawaki Sakura",
-	"img_url": "sakura.jpeg",
+	"img_url": "img/sakura.jpeg",
 	"birthday": "March 19, 1998", 
 	"birthplace": "Kagoshima City, Japan",
 	"position": "Vocalist", 
@@ -26,7 +25,7 @@ let memberDatabase = [
 },
 {
 	"name": "Huh Yunjin",
-	"img_url": "yunjin.jpeg",
+	"img_url": "img/yunjin.jpeg",
 	"birthday": "October 8, 2001", 
 	"birthplace": "Seoul, South Korea (grew up in New York)",
 	"position": "Vocalist", 
@@ -36,7 +35,7 @@ let memberDatabase = [
 }, 
 {
 	"name": "Nakamura Kazuha",
-	"img_url": "kazuha.jpeg",
+	"img_url": "img/kazuha.jpeg",
 	"birthday": "August 9, 2003", 
 	"birthplace": "Kochi, Japan (grew up in Osaka)",
 	"position": "Sub-Vocalist, Rapper, Dancer", 
@@ -47,7 +46,7 @@ let memberDatabase = [
 
 {
 	"name": "Hong Eunchae",
-	"img_url": "eunchae.jpeg",
+	"img_url": "img/eunchae.jpeg",
 	"birthday": "November 10, 2006", 
 	"birthplace": "Miryang, South Korea",
 	"position": "Vocalist, Lead Dancer, Maknae (Youngest)", 
@@ -60,8 +59,6 @@ let memberDatabase = [
 for(var i = 0; i < memberDatabase.length; i++) {
 	updateInfo(memberDatabase[i]);
 }
-
-
 
 function updateInfo(incomingJSON) {
 	// create whole content item div and set class
@@ -79,11 +76,65 @@ function updateInfo(incomingJSON) {
 	// image
 	let newContentImg = document.createElement("IMG");
 	newContentImg.classList.add('memberPic');
-	newContentImg.src = incomingJSON['image'];
+	newContentImg.src = incomingJSON['img_url'];
 
 	newContainer.appendChild(newContentImg);
 
+	// adding a new div into the existing container
 	memberContainer.appendChild(newContainer);
+
+	// content 
+	let contentContainer = document.createElement("DIV");
+	contentContainer.classList.add("rightContainer");
+
+	memberContainer.appendChild(contentContainer);
+
+
+	let newBirthday = document.createElement("P");
+	newBirthday.classList.add("birthday");
+	newBirthday.innerText = "Birthday: " + incomingJSON['birthday'];
+	newContainer.appendChild(newBirthday);
+	contentContainer.appendChild(newBirthday); // adding p into new div
+	// newContainer.appendChild(newBirthday); // adding p into new div
+
+
+
+	let newbirthPlace = document.createElement("P");
+	newbirthPlace.classList.add("birthplace");
+	newbirthPlace.innerText = "Birthplace: " + incomingJSON['birthplace'];
+	contentContainer.appendChild(newbirthPlace); // adding p into new div
+	// newContainer.appendChild(newbirthPlace); // adding p into new div
+
+	let newMBTI = document.createElement("P");
+	newMBTI.classList.add("mbti");
+	newMBTI.innerText = "MBTI: " + incomingJSON['mbti'];
+	contentContainer.appendChild(newMBTI);
+	// newContainer.appendChild(newMBTI); // adding p into new div
+
+
+	let newPosition = document.createElement("P");
+	newPosition.classList.add("position");
+	newPosition.innerText = "Position: " + incomingJSON['position'];
+	contentContainer.appendChild(newPosition);
+	// newContainer.appendChild(newPosition); // adding p into new div
+
+
+	let newNationality = document.createElement("P");
+	newNationality.classList.add("nationality");
+	newNationality.innerText = "Nationality: " + incomingJSON['nationality'];
+	contentContainer.appendChild(newNationality);
+	// newContainer.appendChild(newNationality); // adding p into new div
+
+
+	let newHobby = document.createElement("P");
+	newHobby.classList.add("hobby");
+	newHobby.innerText = "Hobby: " + incomingJSON['hobby'];
+	contentContainer.appendChild(newHobby);
+	// newContainer.appendChild(newHobby); // adding p into new div
+
+
+
+
 }
 
 
